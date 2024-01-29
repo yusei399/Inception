61e9809a64e3307a3840c21224b81f737ee8d736
Ubuntu <ubuntu@ip-172-31-24-155.ec2.internal>
2024-01-29 11:58:19 +0900

    wordpress起動

--- srcs/requirements/mariadb/tools/setup_mariadb.sh
+++ srcs/requirements/mariadb/tools/setup_mariadb.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
 if [ ! -d "/var/lib/mysql/wordpress" ]; then
 
     mysql_install_db
     service mysql start
 
-   mysql -e "CREATE USER '${MYSQL_USER}'@'localhost' identified by '${MYSQL_PASSWORD}';" &&
+    mysql -e "CREATE USER '${MYSQL_USER}'@'localhost' identified by '${MYSQL_PASSWORD}';" &&
         mysql -e "CREATE DATABASE IF NOT EXISTS wordpress;" &&
+	mysql -e "CREATE DATABASE IF NOT EXISTS wordpress CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;" &&
         mysql -e "GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';" &&
         mysql -e "FLUSH PRIVILEGES;"
     mysqladmin -u root password "${MYSQL_ROOT_PASSWORD}"
--- srcs/requirements/wordpress/tools/setup_wordpress.sh
+++ srcs/requirements/wordpress/tools/setup_wordpress.sh
@@ -4,9 +4,18 @@ if [ -f ./wordpress/wp-config.php ]; then
 else
     wp core download --allow-root --locale=ja --force
     sleep 5
-    wp core config --allow-root --dbname=wordpress --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --dbhost=mariadb:3306
+    #wp core config --allow-root --dbname=wordpress --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb:3306
+cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+sed -i "s/database_name_here/${DB_NAME}/g" ./wp-config.php
+sed -i "s/username_here/${DB_USER}/g" ./wp-config.php
+sed -i "s/password_here/${DB_PASSWORD}/g" ./wp-config.php
+sed -i "s/localhost/${DB_HOST}/g" ./wp-config.php
+sed -i "s/define('DB_CHARSET', 'utf8');/define('DB_CHARSET', 'utf8mb4');/" ./wp-config.php
+
+
+
     wp core install --allow-root --url='yuikeda.42.fr' --title='WordPress for Inception' --admin_user=master --admin_password=master --admin_email="admin@admin.fr"
     wp user create --allow-root yuikeda user2@user.com --user_pass=1234567890 --role=author
 fi
 
-php-fpm7.4 --nodaemonize
+php-fpm7.3 --nodaemonize
